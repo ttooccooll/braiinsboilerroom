e1d27cdf3318b9010db77660d2fcbd45fc2f282d
Jason <jasonbohio@gmail.com>
2024-11-19 17:00:25 -0500

    vercel

---
+++ index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+require('dotenv').config();
+
+export default async function handler(req, res) {
+  if (req.url === '/profile') {
+    try {
+      const response = await axios.get('https://pool.braiins.com/accounts/profile/json/btc/', {
+        headers: {
+          'SlushPool-Auth-Token': process.env.API_KEY,
+        },
+      });
+      return res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Error fetching profile data' });
+    }
+  } else if (req.url === '/workers') {
+    try {
+      const response = await axios.get('https://pool.braiins.com/accounts/workers/json/btc/', {
+        headers: {
+          'SlushPool-Auth-Token': process.env.API_KEY,
+        },
+      });
+      return res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Error fetching workers data' });
+    }
+  } else {
+    return res.status(404).json({ message: 'Not found' });
+  }
+}
