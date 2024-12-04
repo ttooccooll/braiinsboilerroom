c307e84bbbaba42ea20ca6feb046828cf090e39c
Jason <jasonbohio@gmail.com>
2024-12-04 10:14:40 -0500

    .env

--- app.mjs
+++ app.mjs
@@ -1,31 +1,29 @@
 import axios from 'axios';
 
+const API_ENDPOINTS = {
+  profile: 'https://pool.braiins.com/accounts/profile/json/btc/',
+  workers: 'https://pool.braiins.com/accounts/workers/json/btc/',
+};
+
 export default async function handler(req, res) {
-  if (req.url === '/profile') {
-    try {
-      const response = await axios.get('https://pool.braiins.com/accounts/profile/json/btc/', {
-        headers: {
-          'SlushPool-Auth-Token': process.env.API_KEY,
-        },
-      });
-      return res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Error fetching profile data' });
-    }
-  } else if (req.url === '/workers') {
-    try {
-      const response = await axios.get('https://pool.braiins.com/accounts/workers/json/btc/', {
-        headers: {
-          'SlushPool-Auth-Token': process.env.API_KEY,
-        },
-      });
-      return res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Error fetching workers data' });
-    }
-  } else {
-    return res.status(404).json({ message: 'Not found' });
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  switch (req.url) {
+    case '/profile':
+    case '/workers':
+      try {
+        const endpoint = API_ENDPOINTS[req.url.slice(1)];
+        const response = await axios.get(endpoint, {
+          headers: {
+            'SlushPool-Auth-Token': apiKey,
+          },
+        });
+        return res.json(response.data);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error fetching data' });
+      }
+    default:
+      return res.status(404).json({ message: 'Not found' });
   }
 }
